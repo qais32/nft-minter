381f0d53168b78c8aa21fb9bf73b6168e969219a
qais32 <108215356+qais32@users.noreply.github.com>
2023-03-21 18:34:33 +0500

    Update App.js
     blockchain.smartContract.methods change mint to _mintLoop

--- nft-minting-app-main/src/App.js
+++ nft-minting-app-main/src/App.js
@@ -130,7 +130,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .mint(blockchain.account, mintAmount)
+      ._mintLoop(blockchain.account, mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
